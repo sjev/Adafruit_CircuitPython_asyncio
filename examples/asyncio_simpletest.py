ba1d4d5f1d4b1a2649c578df4bc68407f10fb076
Jev <jev.kuznetsov@gmail.com>
2024-05-09 21:16:31 +0000

    fill simple test

--- examples/asyncio_simpletest.py
+++ examples/asyncio_simpletest.py
@@ -1,4 +1,23 @@
 # SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
 # SPDX-FileCopyrightText: Copyright (c) 2021 Dan Halbert for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2024 Jev Kuznetsov - ROX Automation
 #
 # SPDX-License-Identifier: Unlicense
+
+""" simple async example """
+
+import asyncio
+
+
+async def loop(id: int, delay: float = 0.1):
+    for i in range(5):
+        print(f"loop {id} : {i}")
+        await asyncio.sleep(delay)
+
+
+async def main():
+    coros = [loop(1), loop(2, 0.5), loop(3, 1.0)]
+    await asyncio.gather(*coros)
+
+
+asyncio.run(main())
